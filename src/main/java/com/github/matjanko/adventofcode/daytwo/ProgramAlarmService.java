7181108acd564574bc42640e1d1fe6725dc130b3
matjanko <mateusz.jankowski@prokonto.pl>
2019-12-03 16:56:26 +0100

    Logic implementation for intcode program

--- src/main/java/com/github/matjanko/adventofcode/daytwo/ProgramAlarmService.java
+++ src/main/java/com/github/matjanko/adventofcode/daytwo/ProgramAlarmService.java
@@ -7,8 +7,28 @@ package com.github.matjanko.adventofcode.daytwo;
 
 public class ProgramAlarmService {
 
-    public int[] doIntcodeProgram(int[] intcode) {
+    public int[] doIntcodeProgram(int[] code) {
+        for (int i = 0; i < code.length; ++i) {
+            if(code[i] == 99) {
+                break;
+            } else if (code[i] == 1) {
+                code[code[i+3]] = code[code[i+1]] + code[code[i+2]];
+                i += 3;
+            } else if (code[i] == 2) {
+                code[code[i+3]] = code[code[i+1]] * code[code[i+2]];
+                i += 3;
+            }
+        }
+        return code;
+    }
 
-        return new int[]{0,0,0,0,0};
+    public int[] prepareIntcodeProgram(int[] code) {
+        code[1] = 12;
+        code[2] = 2;
+        return code;
+    }
+
+    public int getResult(int[] code) {
+        return code[0];
     }
 }
